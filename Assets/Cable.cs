0705373d052c2c57e15d0c61d656cd438faae962
Antoine Villecroze <antoine.villecroze@gmail.com>
2019-10-04 22:53:56 +0200

    first

---
+++ Assets/Cable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cable : MonoBehaviour
+{
+
+    public BezierSpline spline;
+    public LineRenderer lr;
+
+    public int resolution;
+
+    public int pointIndex;
+    public Vector3 PIpos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        spline.SetControlPoint(pointIndex, PIpos);
+    }
+
+    public void UpdateLine()
+    {
+
+    }
+}
